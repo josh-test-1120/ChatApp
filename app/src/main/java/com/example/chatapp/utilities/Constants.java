d5214bb8e1ebd01af7ddf5aaafa10942423d9799
Joshua Summers <joshua.summers@cwu.edu>
2024-11-14 20:44:59 -0800

    Initial Chat Application Synchronization

---
+++ app/src/main/java/com/example/chatapp/utilities/Constants.java
@@ -0,0 +1,16 @@
+package com.example.chatapp.utilities;
+
+/**
+ * Constants model
+ */
+public class Constants {
+    // Hash keys for database contracts
+    public static final String KEY_COLLECTION_USERS = "User";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_USERID = "userid";
+    public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
+    public static final String KEY_IMAGE = "image";
+}
